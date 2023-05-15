ccf0b1359c3e1917024359ed5195e65906af2c3c
Harikrashna <hparikh@argusoft.com>
2023-05-15 19:06:32 +0530

    Parent Child working for Actions succesfully

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -12,6 +12,7 @@ export class GraphComponent implements OnInit {
   i: number = 0;
   children=[];  
   data!: TreeNode[] ;
+  allData: any= [];
   constructor(private graphService: GraphServiceService) {}
   ngOnInit() {
     this.graphService.getPlanDefinition().subscribe((response: any) => {
@@ -25,59 +26,38 @@ export class GraphComponent implements OnInit {
           data: {
             id: 'Plan Definition',
           },
-          children: [
-            {
-              expanded: true,
-              type: 'person',
-              styleClass: 'bg-purple-500 text-white ',
-              data: {
-                id: this.childData(this.i,this.posts),
-              },
-              children: [
-                {
-                  label: 'Sales',
-                  styleClass: 'bg-purple-500 text-white',
-                  style: ' border-radius: 12px',
-                },
-                {
-                  label: 'Marketing',
-                  styleClass: 'bg-purple-500 text-white',
-                  style: ' border-radius: 12px',
-                },
-              ],
-            },
-            {
-              expanded: true,
-              type: 'person',
-              styleClass: 'bg-teal-500 text-white',
-              data: {
-                id: this.childData(this.i,this.posts),
-
-              },
-              children: [
-                {
-                  label: 'Development',
-                  styleClass: 'bg-teal-500 text-white',
-                },
-                {
-                  label: 'UI/UX Design',
-                  styleClass: 'bg-teal-500 text-white',
-                },
-              ],
-            },
-          ],
+          children: [],
         },
       ];
+      this.allData = response.action
+      console.log("FDSFDSHDOHFDSIHDFDFSIH......."+this.allData[0].id);
+      for(let data of this.allData){
+        this.data[0].children?.push({
+          expanded: true,
+          type: 'person',
+          styleClass: 'bg-blue-500 text-white',
+          data: {
+            id: data.id,
+            description: data.description,
+            definitionCanonical: data.definitionCanonical
+          }
+        })
+      }
     });
-  }
-  childData(i: number,posts: any) {
-    let id = posts.action[i].id;
-    console.log("inside function id value : "+id)
-    this.i=++i;
-    console.log(this.i);
-    
-    return id;
-  }
 
+    
+  }
+  // childData(i: number,posts: any) {
+  //   let id = posts.action[i].id;
+  //   console.log("inside function id value : "+id)
+  //   this.i=++i;
+  //   console.log(this.i);
+    
+  //   return id;
+  // }
+
+  
+
+  
 
 } 
