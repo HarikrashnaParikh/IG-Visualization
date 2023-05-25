16745a3defe8bcd2bc3f67f0191eb099325f4d79
Harikrashna <hparikh@argusoft.com>
2023-05-25 17:20:16 +0530

    Added Color feature using Bootstrap and also added new get methods

--- src/app/graph-service.service.ts
+++ src/app/graph-service.service.ts
@@ -28,15 +28,39 @@ export class GraphServiceService {
     return this.httpClient.get(`${this.url}/activityDefinition/`+activityDefinitionId);
   }
 
+  getQuestionnaire(){
+    return this.httpClient.get(`${this.url}/questionnaire`);
+  }
+
   getQuestionnaireByID(activityDefinitionId: any){
     return this.httpClient.get(`${this.url}/questionnaire/`+activityDefinitionId);
   }
 
+  getStructureMap(){
+    return this.httpClient.get(`${this.url}/structureMap`);
+  }
+  
   getStructureMapByID(activityDefinitionId: any){
     return this.httpClient.get(`${this.url}/structureMap/`+activityDefinitionId);
   }
   
   getTargetById(activityDefinitionId: any){
     return this.httpClient.get(`${this.url}/targetFromStructureMap/`+activityDefinitionId);
+  }  
+
+  getStructureDefinition(){
+    return this.httpClient.get(`${this.url}/structureDefinition`);
+  }
+
+  getLibrary(){
+    return this.httpClient.get(`${this.url}/library`);
+  }
+
+  getValueSet(){
+    return this.httpClient.get(`${this.url}/valueSet`);
+  }
+
+  getCode(){
+    return this.httpClient.get(`${this.url}/code`);
   }
 }
--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {} from 'primeng';
 import { TreeNode } from 'primeng/api';
 import { GraphServiceService } from '../graph-service.service';
 import { forkJoin } from 'rxjs';
@@ -8,21 +7,19 @@ import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-graph',
   templateUrl: './graph.component.html',
+  styleUrls: ['./graph.component.css'], // Add this line to link the CSS file
 })
 export class GraphComponent implements OnInit {
-  posts: any = [];
-  activityDefinition: any = [];
-  Questionnaire: any = [];
-  StructureMap: any = []; // New data for StructureMap
-  i: number = 0;
-  j: number = 0;
-  k: number = 0; // New counter for StructureMap
-  children = [];
   data!: TreeNode[];
-  allActions: any = [];
-  Target: any = [];
+  actions: any = [];
+  ad: any = [];
+  questionnaire: any = [];
+  structureMap: any =[];
+  structureDefinition: any = [];
+  library: any = [];
+  valueSet: any = [];
+  code: any = [];
 
-  a = 1;
   constructor(private graphService: GraphServiceService) {}
 
   ngOnInit() {
@@ -30,15 +27,15 @@ export class GraphComponent implements OnInit {
       .getPlanDefinition()
       .pipe(
         switchMap((response: any) => {
-          this.posts = response;
+          const planDefinitionId = response.resource.id;
           this.data = [
             {
               expanded: true,
               type: 'person',
-              styleClass: 'bg-indigo-500 text-white',
+              styleClass: 'bg-primary text-white',
               data: {
                 name: 'Plan Definition',
-                id: this.posts.resource.id,
+                id: planDefinitionId,
               },
               children: [],
             },
@@ -47,33 +44,35 @@ export class GraphComponent implements OnInit {
           return this.graphService.getActions();
         }),
         switchMap((response: any) => {
-          this.allActions = response;
+          this.actions = response;
+          const actionsObservables = this.actions.map((action: any) => {
+            const actionId = action.id;
+            const actionDescription = action.description;
+            const actionDefinitionCanonical = action.definitionCanonical;
 
-          const actionsObservables = this.allActions.map((action: any) => {
             this.data[0].children?.push({
               expanded: false,
               type: 'person',
-              styleClass: 'bg-blue-500 text-white',
+              styleClass: 'bg-warning text-dark',
               data: {
                 name: 'Action',
-                id: action.id,
-                description: action.description,
-                definitionCanonical: action.definitionCanonical,
+                id: actionId,
+                description: actionDescription,
+                definitionCanonical: actionDefinitionCanonical,
               },
               children: [],
             });
 
-            const idStartIndex: number =
-              action.definitionCanonical.lastIndexOf('/') + 1;
-            const activityDefinitionId: string =
-              action.definitionCanonical.substr(idStartIndex);
+            const idStartIndex = actionDefinitionCanonical.lastIndexOf('/') + 1;
+            const activityDefinitionId =
+              actionDefinitionCanonical.substr(idStartIndex);
 
             const activityDefinition$ =
               this.graphService.getActivityDefinitionById(activityDefinitionId);
             const questionnaire$ =
               this.graphService.getQuestionnaireByID(activityDefinitionId);
             const structureMap$ =
-              this.graphService.getStructureMapByID(activityDefinitionId); // New API call for StructureMap
+              this.graphService.getStructureMapByID(activityDefinitionId);
             const target$ =
               this.graphService.getTargetById(activityDefinitionId);
 
@@ -97,74 +96,268 @@ export class GraphComponent implements OnInit {
               structureMapResponse,
               targetResponse,
             ] = responses[i];
-            this.activityDefinition = activityDefinitionResponse;
-            this.Questionnaire = questionnaireResponse;
-            this.StructureMap = structureMapResponse; // Assign the StructureMap response to the corresponding property
-            this.Target = targetResponse;
+            const activityDefinition = activityDefinitionResponse;
+            const questionnaire = questionnaireResponse;
+            const structureMap = structureMapResponse;
+            const target = targetResponse;
 
-            const structureMapChildren = []; // Array to hold multiple children for StructureMap
+            const structureMapChildren = [];
 
-            // Add multiple children for StructureMap data
-            for (let j = 0; j < this.Target.length; j++) {
-              const targetItem = this.Target[j];
+            for (let j = 0; j < target.length; j++) {
+              const targetItem = target[j];
               structureMapChildren.push({
                 expanded: false,
                 type: 'person',
-                styleClass: 'bg-blue-500 text-white',
+                styleClass: 'bg-light text-dark',
                 data: {
                   name: 'Target',
                   id: targetItem.alias,
                 },
-                children: [], // You can add additional children for each target item if needed
+                children: [],
               });
             }
 
             this.data[0].children[i].children.push({
               expanded: false,
               type: 'person',
-              styleClass: 'bg-blue-500 text-white',
+              styleClass: 'bg-success text-white',
               data: {
                 name: 'Activity Definition',
-                id: this.activityDefinition.resource.id,
-                description: this.activityDefinition.description,
-                definitionCanonical:
-                  this.activityDefinition.definitionCanonical,
+                id: activityDefinition.resource.id,
+                description: activityDefinition.description,
+                definitionCanonical: activityDefinition.definitionCanonical,
               },
               children: [
                 {
                   expanded: false,
                   type: 'person',
-                  styleClass: 'bg-blue-500 text-white',
+                  styleClass: 'bg-secondary text-white',
                   data: {
                     name: 'Questionnaire',
-                    id: this.Questionnaire.resource.id,
+                    id: questionnaire.resource.id,
                   },
                   children: [
                     {
                       expanded: false,
                       type: 'person',
-                      styleClass: 'bg-blue-500 text-white',
+                      styleClass: 'bg-danger text-white',
                       data: {
                         name: 'Structure Map',
-                        id: this.StructureMap.resource.id,
+                        id: structureMap.resource.id,
                       },
-                      children: structureMapChildren, // Assign the structureMapChildren array to the children property
+                      children: structureMapChildren,
                     },
                   ],
                 },
               ],
             });
-
-            console.log(this.Questionnaire.resource.id);
-            console.log(this.StructureMap.resource.id); // Log the StructureMap ID
           }
         },
         (error) => {
           console.log(error);
         }
       );
+
+    this.graphService.getActivityDefinition().subscribe((response: any) => {
+      this.ad = response; // Handle the response
+    });
+
+    this.graphService.getQuestionnaire().subscribe((response: any) => {
+      this.questionnaire = response; // Handle the response
+    });
+
+    this.graphService.getStructureMap().subscribe((response: any) => {
+      this.structureMap = response; // Handle the response
+    });
+
+    this.graphService.getStructureDefinition().subscribe((response: any) => {
+      this.structureDefinition = response; // Handle the response
+    });
+
+    this.graphService.getLibrary().subscribe((response: any) => {
+      this.library = response; // Handle the response
+    });
+
+    this.graphService.getValueSet().subscribe((response: any) => {
+      this.valueSet = response; // Handle the response
+    });
+
+    this.graphService.getCode().subscribe((response: any) => {
+      this.code = response; // Handle the response
+    });
   }
 }
+
+// import { Component, OnInit } from '@angular/core';
+// import {} from 'primeng';
+// import { TreeNode } from 'primeng/api';
+// import { GraphServiceService } from '../graph-service.service';
+// import { forkJoin } from 'rxjs';
+// import { switchMap } from 'rxjs/operators';
+
+// @Component({
+//   selector: 'app-graph',
+//   templateUrl: './graph.component.html',
+//   styleUrls: ['./graph.component.css']
+// })
+// export class GraphComponent implements OnInit {
+//   posts: any = [];
+//   activityDefinition: any = [];
+//   Questionnaire: any = [];
+//   StructureMap: any = []; // New data for StructureMap
+//   i: number = 0;
+//   j: number = 0;
+//   k: number = 0; // New counter for StructureMap
+//   children = [];
+//   data!: TreeNode[];
+//   Target: any = [];
+
+//   a = 1;
+//   constructor(private graphService: GraphServiceService) {}
+
+//   ngOnInit() {
+//     this.graphService
+//       .getPlanDefinition()
+//       .pipe(
+//         switchMap((response: any) => {
+//           this.posts = response;
+//           this.data = [
+//             {
+//               expanded: true,
+//               type: 'person',
+//               styleClass: 'bg-indigo-500 text-white',
+//               data: {
+//                 name: 'Plan Definition',
+//                 id: this.posts.resource.id,
+//               },
+//               children: [],
+//             },
+//           ];
+
+//           return this.graphService.getActions();
+//         }),
+//         switchMap((response: any) => {
+//           this.allActions = response;
+
+//           const actionsObservables = this.allActions.map((action: any) => {
+//             this.data[0].children?.push({
+//               expanded: false,
+//               type: 'person',
+//               styleClass: 'bg-blue-500 text-white',
+//               data: {
+//                 name: 'Action',
+//                 id: action.id,
+//                 description: action.description,
+//                 definitionCanonical: action.definitionCanonical,
+//               },
+//               children: [],
+//             });
+
+//             const idStartIndex: number =
+//               action.definitionCanonical.lastIndexOf('/') + 1;
+//             const activityDefinitionId: string =
+//               action.definitionCanonical.substr(idStartIndex);
+
+//             const activityDefinition$ =
+//               this.graphService.getActivityDefinitionById(activityDefinitionId);
+//             const questionnaire$ =
+//               this.graphService.getQuestionnaireByID(activityDefinitionId);
+//             const structureMap$ =
+//               this.graphService.getStructureMapByID(activityDefinitionId); // New API call for StructureMap
+//             const target$ =
+//               this.graphService.getTargetById(activityDefinitionId);
+
+//             return forkJoin([
+//               activityDefinition$,
+//               questionnaire$,
+//               structureMap$,
+//               target$,
+//             ]);
+//           });
+
+//           return forkJoin(actionsObservables);
+//         })
+//       )
+//       .subscribe(
+//         (responses: any) => {
+//           for (let i = 0; i < responses.length; i++) {
+//             const [
+//               activityDefinitionResponse,
+//               questionnaireResponse,
+//               structureMapResponse,
+//               targetResponse,
+//             ] = responses[i];
+//             this.activityDefinition = activityDefinitionResponse;
+//             this.Questionnaire = questionnaireResponse;
+//             this.StructureMap = structureMapResponse; // Assign the StructureMap response to the corresponding property
+//             this.Target = targetResponse;
+
+//             const structureMapChildren = []; // Array to hold multiple children for StructureMap
+
+//             // Add multiple children for StructureMap data
+//             for (let j = 0; j < this.Target.length; j++) {
+//               const targetItem = this.Target[j];
+//               structureMapChildren.push({
+//                 expanded: false,
+//                 type: 'person',
+//                 styleClass: 'bg-blue-500 text-white',
+//                 data: {
+//                   name: 'Target',
+//                   id: targetItem.alias,
+//                 },
+//                 children: [], // You can add additional children for each target item if needed
+//               });
+//             }
+
+//             this.data[0].children[i].children.push({
+//               expanded: false,
+//               type: 'person',
+//               styleClass: 'bg-blue-500 text-white',
+//               data: {
+//                 name: 'Activity Definition',
+//                 id: this.activityDefinition.resource.id,
+//                 description: this.activityDefinition.description,
+//                 definitionCanonical:
+//                   this.activityDefinition.definitionCanonical,
+//               },
+//               children: [
+//                 {
+//                   expanded: false,
+//                   type: 'person',
+//                   styleClass: 'bg-blue-500 text-white',
+//                   data: {
+//                     name: 'Questionnaire',
+//                     id: this.Questionnaire.resource.id,
+//                   },
+//                   children: [
+//                     {
+//                       expanded: false,
+//                       type: 'person',
+//                       styleClass: 'bg-blue-500 text-white',
+//                       data: {
+//                         name: 'Structure Map',
+//                         id: this.StructureMap.resource.id,
+//                       },
+//                       children: structureMapChildren, // Assign the structureMapChildren array to the children property
+//                     },
+//                   ],
+//                 },
+//               ],
+//             });
+
+//             console.log(this.Questionnaire.resource.id);
+//             console.log(this.StructureMap.resource.id); // Log the StructureMap ID
+//           }
+//         },
+//         (error) => {
+//           console.log(error);
+//         }
+//       );
+//       this.graphService.getActivityDefinition().subscribe((response: any)=>{
+//         this.ad=response
+//       })
+//   }
+// }
 
 // import { Component, OnInit } from '@angular/core';
 // import {} from 'primeng';
