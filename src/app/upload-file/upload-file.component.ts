4c13d3829998dd42cf625bd53849e4f7971bc312
Harikrashna <hparikh@argusoft.com>
2023-05-25 09:43:27 +0530

    Code Formatted

--- src/app/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file.component.ts
@@ -13,31 +13,26 @@ export class UploadFileComponent {
 
   constructor(private http: HttpClient, private router: Router) {}
 
-  onSubmit() {
-    console.log('Hello');
-    // const formData = new FormData();
-    // formData.append("file",this.file);
-    // this.http.post('http://localhost:8080/file-upload',formData).subscribe(
-    //   (response) => console.log(response),
-    //   (error) => console.log(error)
-    // );
-    
+     onSubmit() {
     if (this.file) {
       const formData: FormData = new FormData();
+      console.log(formData);
+      
       formData.append('bundle', this.file, this.file.name);
+      console.log(formData);
 
       this.http.post('http://localhost:8080/file-upload', formData)
-        .subscribe(
-          (response) => {
+        .subscribe({
+          next: (response) => {
             console.log(response);
             // Handle success message or redirect to another page
-          },
-          (error) => {
+          }, error: (error) => {
+            console.log("ABC");
             console.error('Error occurred during file upload:', error);
             // Handle error message or display an error alert
           }
-          );
-          this.router.navigate(['/visualization']);
+        });
+        this.router.navigate(['/visualization']);
     }
 
   }
